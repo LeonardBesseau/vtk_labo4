a780a9d08216b39d903b3806b3a44b5df1400882
LeonardBesseau <leonard.besseau@heig-vd.ch>
2022-05-31 16:44:56 +0200

    No idea what's heppening

--- main.py
+++ main.py
@@ -15,12 +15,22 @@ if __name__ == '__main__':
     texture.RepeatOff()
     texture.SetInputConnection(reader.GetOutputPort())
 
+    color_table = vtk.vtkColorTransferFunction()
+    sea_level = 0
+    color_table.AddRGBPoint(sea_level + 0, 0.52, 0.5, 1)
+    color_table.AddRGBPoint(sea_level + 494, 0.129, 0.643, 0.318)
+    color_table.AddRGBPoint(sea_level + 500, 0.573, 0.765, 0.431)
+    color_table.AddRGBPoint(sea_level + 600, 0.573, 0.765, 0.431)
+    color_table.AddRGBPoint(sea_level + 800, 0.949, 0.864, 0.808)
+    color_table.AddRGBPoint(sea_level + 1200, 1, 1, 1)
+
     grid = read_elevation(ELEVATION_FILE)
     grid_mapper = vtk.vtkDataSetMapper()
     grid_mapper.SetInputData(grid)
+    grid_mapper.SetLookupTable(color_table)
     michel = vtk.vtkActor()
     michel.SetMapper(grid_mapper)
-    michel.SetTexture(texture)
+    #michel.SetTexture(texture)
 
     renderer = vtk.vtkRenderer()
     renderer.AddActor(michel)
--- parser.py
+++ parser.py
@@ -13,6 +13,8 @@ ELEVATION_LIMITS_WGS84 = [(60, 10), (65, 15)]
 ELEVATION_DATA_DIMENSIONS = (6000, 6000)
 EARTH_RADIUS = 6371009
 
+QUAD_INTERPOLATION_MATRIX = np.array([[1, 0, 0, 0], [-1, 1, 0, 0], [-1, 0, 0, 1], [1, -1, 1, -1]])
+
 
 def convert_RT90_list_toWGS84(RT90_list):
     return [RT90_TO_WGS84.transform(x[1], x[0]) for x in RT90_list]
@@ -20,29 +22,21 @@ def convert_RT90_list_toWGS84(RT90_list):
 
 def convert_to_cartesian(longitude, latitude, elevation):
     transform = vtk.vtkTransform()
-    transform.RotateX(-longitude)
-    transform.RotateY(latitude)
-    transform.Translate(0, 0, elevation)
-    return transform.TransformPoint((0, 0, 0))
+    transform.RotateX(latitude)
+    transform.RotateY(longitude)
+    return transform.TransformPoint((0, 0, elevation))
 
 
-def polar_to_cartesian(x, y, z) -> (float, float, float):
-    """
-    Convert polar coordinates to cartesian coordinates
-    See https://en.wikipedia.org/wiki/Spherical_coordinate_system#Cartesian_coordinates
-    :param x: longitude
-    :param y: latitude
-    :param z: altitude
-    :return:
-    """
-    x = math.radians(x)
-    y = math.radians(y)
-    z = math.radians(z)
-    X = z * math.sin(x) * math.cos(y)
-    Y = z * math.cos(x) * math.cos(y)
-    Z = z * math.sin(y)
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
 
-    return X, Y, Z
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
 
 
 def read_elevation(elevation_file):
@@ -57,26 +51,26 @@ def read_elevation(elevation_file):
     elevation_value = vtk.vtkIntArray()
     texture_coords = vtk.vtkFloatArray()
     texture_coords.SetNumberOfComponents(2)
-    rows = 0
-    cols = 0
+
     for longitude in range(ELEVATION_DATA_DIMENSIONS[1]):
         if 12.802148375908851 <= longitude_coords[longitude] <= 13.262723166975816:
             for latitude in range(ELEVATION_DATA_DIMENSIONS[0]):
                 if 63.13203595611243 <= latitude_coords[latitude] <= 63.29286525347288:
-                    elevation = data[longitude * ELEVATION_DATA_DIMENSIONS[1] + latitude]
-                    p = convert_to_cartesian(longitude_coords[longitude], latitude_coords[latitude], EARTH_RADIUS + elevation)
+                    elevation = data[latitude + longitude * ELEVATION_DATA_DIMENSIONS[1]]
+                    p = convert_to_cartesian(longitude_coords[longitude], latitude_coords[latitude],
+                                             EARTH_RADIUS + elevation)
                     elevation_points.InsertNextPoint(p)
                     elevation_value.InsertNextValue(elevation)
-                    x = longitude_coords[longitude] * 1024/13.262723166975816
-                    y = latitude_coords[latitude] * 768/63.29286525347288
-                    texture_coords.InsertNextTuple2(x,y)
+                    x = translate(longitude_coords[longitude], 12.802148375908851, 13.262723166975816, 0, 1024)
+                    y = translate(latitude_coords[latitude], 63.13203595611243, 63.29286525347288, 0, 768)
+                    texture_coords.InsertNextTuple2(x, y)
 
-    for i in longitude_coords:
-        if 12.802148375908851 <= i <= 13.262723166975816:
-            cols += 1
-    for i in latitude_coords:
-        if 63.13203595611243 <= i <= 63.29286525347288:
-            rows += 1
+    a = longitude_coords >= 12.802148375908851
+    b = longitude_coords <= 13.262723166975816
+    cols = longitude_coords[a & b].size
+    c = latitude_coords >= 63.13203595611243
+    d = latitude_coords <= 63.29286525347288
+    rows = latitude_coords[c & d].size
 
     limits_wgs84 = convert_RT90_list_toWGS84(MAP_LIMITS_RT90)
 
